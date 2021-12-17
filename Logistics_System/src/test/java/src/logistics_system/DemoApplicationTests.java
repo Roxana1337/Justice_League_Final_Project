9b9989d59f17ad59f31352ab9d96c3c4ba0dcc24
Andrei <rusandrei95@gmail.com>
2021-12-17 22:40:42 +0200

    initial 2

---
+++ Logistics_System/src/test/java/src/logistics_system/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package src.logistics_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
